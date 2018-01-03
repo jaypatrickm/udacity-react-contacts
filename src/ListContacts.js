ce8e59a001bf95d644a0926e7d2f70d09c0aae32
Jaypatrick <JaypatrickM@gmail.com>
2018-01-02 22:07:33 -0800

    adding State

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
-class ListContacts extends Component {
-  render() {
-  	console.log('Props', this.props)
-    return (
+function ListContacts(props) {
+	return (
       <ol className='contact-list'>
-
+      	{props.contacts.map((contact) => (
+  			<li key={contact.id} className='contact-list-item'>
+  				<div className='contact-avatar' style={{
+  					backgroundImage: `url(${contact.avatarURL})`
+  				}}/>
+  				<div className='contact-details'>
+  					<p>{contact.name}</p>
+  					<p>{contact.email}</p>
+  				</div>
+  				<button className='contact-remove'>
+					Remove
+  				</button>
+  			</li>
+      		))}
       </ol>
     )
-  }
 }
 
 export default ListContacts;
