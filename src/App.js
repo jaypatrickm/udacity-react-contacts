ba3fb198f9c7ef81a485345006988c6382fc3df8
Jaypatrick <JaypatrickM@gmail.com>
2018-01-07 21:50:52 -0800

    Adding CreateContact page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import ListContacts from './ListContacts';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
+    screen: 'create', //list, create
     contacts : []
   }
 
@@ -24,9 +26,14 @@ class App extends Component {
   render() {
     return (
       <div>
+      {this.state.screen === 'list' && (
         <ListContacts 
           onDeleteContact={this.removeContact} 
           contacts={this.state.contacts} />
+        )}
+      {this.state.screen === 'create' && (
+         <CreateContact />
+      )}
       </div>
     )
   }
